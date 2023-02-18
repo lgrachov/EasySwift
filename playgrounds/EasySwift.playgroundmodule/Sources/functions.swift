57c00846826b333de52525c92a9adb48b414aa62
Leo Grachov <78234184+lgrachov@users.noreply.github.com>
2023-02-18 13:41:59 +0200

    init

---
+++ playgrounds/EasySwift.playgroundmodule/Sources/functions.swift
@@ -0,0 +1,11 @@
+func pickRandom() {
+    var pickRandomResult = Int.random(in: pickRandomFrom..<pickRandomTo)
+}
+func printToConsole() {
+    print(printToConsoleText)
+}
+//func variable() {
+//    var  = 
+//}
+
+// idk how to make variable creator, use var name = "contents"
---
+++ playgrounds/EasySwift.playgroundmodule/Sources/variables.swift
@@ -0,0 +1,10 @@
+var yes = true
+var no = false
+var pickRandomFrom = 0
+var pickRandomTo = 0
+var pickRandomResult = 0
+var printToConsoleText = ""
+//var variableName = ""
+//var variableContent = ""
+
+// idk how to make variable creator, use var name = "contents"
